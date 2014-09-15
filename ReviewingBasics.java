5fd952ec4d8ae14f811fafaa57d3b8f5da1f1638
Mr.M <morrisv@harrisoncsd.org>
2014-09-15 14:56:25 -0400

    review basics strings

---
+++ ReviewingBasics.java
@@ -0,0 +1,22 @@
+
+public class ReviewingBasics
+{
+    public ReviewingBasics()
+    {
+	boolean b =false;
+	boolean b2 = false;
+	
+	int x =5;
+	int y = 5;
+
+String f ="f";
+String f2 = new String("f");
+String f3 = f;
+System.out.println(f ==f2) ;
+
+System.out.println(f3 ==f) ;
+
+	
+	
+	}
+}
